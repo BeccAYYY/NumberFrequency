8ad5f1d9fc559f7fdbacb44d46a6e6990c334402
becca <becsell@live.com.au>
2023-11-23 07:29:07 +1000

    take input and return FIB! if fibonacci

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,21 @@
 import { FibonacciChecker } from './services/FibonacciChecker';
+import { NumberRecorderClass } from './services/NumberRecorderClass';
 
 
-class PerformanceTest {
-    private readonly numbersToCheck: bigint[] = [
-      987n,
-      987n,
-      987n,
-      43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875n,
-      399999999999999n,
-      1n,
-      987n,
-    ];
-  
-    private readonly iterations: number = 1000000;
-  
-    runTest(checker: NumberChecker, testName: string): void {
-      for (const numberToCheck of this.numbersToCheck) {
-        const startTime = performance.now();
-  
-        for (let i = 0; i < this.iterations; i++) {
-          checker.meetsCriteria(numberToCheck);
-        }
-  
-        const endTime = performance.now();
-        const duration = endTime - startTime;
-  
-        console.log(`${testName} took ${duration.toFixed(2)} milliseconds for ${this.iterations} iterations with numberToCheck: ${numberToCheck}`);
-      }
-    }
+const prompt = require('prompt-sync')();
+
+const fibonacciChecker = new FibonacciChecker();
+const numberRecorder = new NumberRecorderClass(fibonacciChecker)
+
+const userInput = prompt("Enter a number: ");
+const nonNullUserInput = userInput == null ? '' : userInput;
+
+function checkAndPrintResult(input: string): void {
+  const bigIntput = BigInt(input);
+
+  if (numberRecorder.checkAndRecordNumber(bigIntput)) {
+    console.log("FIB!");
   }
-  
+};
 
-  const performanceTest = new PerformanceTest();
-  const generatedListChecker = new FibonacciChecker();
-
-  performanceTest.runTest(generatedListChecker, 'Generated List Fibonacci Checker');
+checkAndPrintResult(nonNullUserInput);
